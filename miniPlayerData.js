17e7270292a5906c0d9bfa43cb0a351cd19e7408
Yangwoo Kim <yangwookim2@gmail.com>
2023-05-28 19:19:49 +0900

    Create miniPlayerData.js

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "resumePlaybackWording": "Resume playback?",
+    "launchAppWording": "Then Launch Project",
+    "yesWording": "Yes",
+    "downloadAppWording": "Download App",
+    "description": "Air Flow Sound Web video work ",
+    "title": "Air Flow Sound",
+    "noWording": "No",
+    "thumbnail": "Player/css/editor/img/mini-player.jpg",
+    "url": "www.airflowsound.com",
+    "redirectToMobileApp": "never",
+    "analyticsKey": "",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "thanksForWatchingWording": "Thanks for watching!"
+}
